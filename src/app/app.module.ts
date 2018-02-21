bfb69e0b7da712fe91317dc8fdaba73b50978d64
Diego Guzmán <dfguzman@ceaaces.gob.ec>
2018-02-21 14:56:02 -0500

    pipe de fecha con región del país y pipe personalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Para trabajar con pipe de fechas con region de pais para que no esté por defecto en-US
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-EC';
 
 import { AppComponent } from './app.component';
+//importación de pipe personalizado
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
 
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-EC' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
