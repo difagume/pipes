bd6dfb42dc87d92f77b5de13b13368138d891f0a
Diego Guzmán <dfguzman@ceaaces.gob.ec>
2018-02-21 17:01:55 -0500

    pipes personalizados

---
+++ src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contrasena'
+})
+export class ContrasenaPipe implements PipeTransform {
+
+  transform(value: string, activar: boolean = true): string {
+    if (activar) {
+      return value.replace(/./g, '*');
+    }
+    else return value;
+  }
+
+}
