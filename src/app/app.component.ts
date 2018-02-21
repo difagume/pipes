516ffc73d52d8845a5f1b696ab6d0265d43661d4
Diego Guzmán <dfguzman@ceaaces.gob.ec>
2018-02-21 17:02:11 -0500

    uso de pipes para dar formato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = "Diego";
-  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  nombre2="dieGo fabrIcio guzmáN medraNo";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  video = "fQkos0_NwV0";
+  activar:boolean = true;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Llegó la data'), 3500);
+  })
+
+  fecha = new Date();
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import localeEs from '@angular/common/locales/es-EC';
 import { AppComponent } from './app.component';
 //importación de pipe personalizado
 import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
     AppComponent,
-    CapitalizadoPipe
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
